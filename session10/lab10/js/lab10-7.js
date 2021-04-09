aeaff00339f1da765461ae59f3f562defbca4780
huyhuanhg <huyhuanhg@gmail.com>
2021-04-09 23:21:23 +0700

    update param of lab10-1

--- session10/lab10/js/lab10-7.js
+++ session10/lab10/js/lab10-7.js
@@ -1,62 +1,61 @@
 var answerBtn = document.querySelectorAll('.answer-btn');
 var functionQuesstion = {
-    question1: function () {
-        let people = 12 * 2 - 4;
+    question1: function (params) {
+        let people = params[0] * params[2] - params[1];
         return `Số công nhân tổ 2 là: ${people} người`;
     },
-    question2: function () {
-        let money = 2000;
-        let price = 200;
+    question2: function (params) {
+        let money = params[0];
+        let price = params[1];
         let candy = money / price;
         let candyShell = candy;
-        while (candyShell >= 2) {
-            candy += Math.floor(candyShell / 2);
-            candyShell = Math.floor(candyShell / 2) + (candyShell % 2);
+        while (candyShell >= params[2]) {
+            candy += Math.floor(candyShell / params[2]);
+            candyShell = Math.floor(candyShell / params[2]) + (candyShell % params[2]);
         }
         return `Số kẹo có thể đổi được là ${candy} cái`;
     },
-    question3: function () {
-        let marbles1 = 18;
-        let marbles2 = marbles1 * 2;
+    question3: function (params) {
+        let marbles1 = params[0];
+        let marbles2 = marbles1 * params[1];
         let count = 0;
         while (marbles2-- > marbles1++) {
             count++;
         }
         return (--marbles1 == ++marbles2) ? `Cần chuyển ${count} viên bi để 2 túi có số bi bằng nhau` : 'Số bi trong 2 túi luôn luôn chênh lệch';
     },
-    question4: function () {
-        let bookOfBinh = 27;
-        let bookOfMinh = bookOfBinh / 3;
+    question4: function (params) {
+        let bookOfBinh = params[0];
+        let bookOfMinh = bookOfBinh / params[1];
         let count = 0;
-        while (bookOfBinh-- > bookOfMinh++ * 2) {
+        while (bookOfBinh-- > bookOfMinh++ * params[2]) {
             count++;
         }
         return `Bình cần cho Minh ${count} cuốn sách`;
     },
-    question5: function () {
-        let totalMarbles = 50;
+    question5: function (params) {
+        let totalMarbles = Number(params[0]);
         let redMarbles = totalMarbles + 1;
         let blueMarbles = 0;
         for (blueMarbles; blueMarbles <= totalMarbles; blueMarbles++) {
-            if (--redMarbles * 3 / 4 + blueMarbles * 2 / 5 === 27)
+            if (--redMarbles * Number(params[2]) + blueMarbles * Number(params[1]) === Number(params[3]))
                 return `Số bi xanh là: ${blueMarbles}, bi đỏ là: ${redMarbles}`;
-
         }
     },
-    question6: function () {
-        let totalMarbles = 120;
-        let redMarbles = totalMarbles / 5;
-        let blueMarbles = totalMarbles * 3 / 10;
+    question6: function (params) {
+        let totalMarbles = params[0];
+        let redMarbles = totalMarbles / params[1];
+        let blueMarbles = totalMarbles * params[2] / 100;
         let overMarbles = totalMarbles - redMarbles - blueMarbles;
         let whiteMarbles = 0;
         let yellowMarbles = overMarbles + 1;
         for (whiteMarbles; whiteMarbles <= overMarbles; whiteMarbles++) {
-            if (whiteMarbles * 7 / 3 === --yellowMarbles)
+            if (whiteMarbles * params[4] / params[3] === --yellowMarbles)
                 return `Đỏ: ${redMarbles}, Xanh: ${blueMarbles}, Vàng: ${yellowMarbles}, Trắng: ${whiteMarbles}`;
         }
     },
-    question7: function () {
-        var candy = 50;
+    question7: function (params) {
+        var candy = params[0];
         var euro = 0;
         var usd = 0;
         // 1. trước khi đến kẹo = 0, euro = 0
@@ -68,11 +67,11 @@ var functionQuesstion = {
         // cách 1: đệ quy
         function getUsd (candy, euro, dollar){
             if (euro <= 2) {
-                euro += 2;
-                dollar -= 3;
+                euro += Number(params[1]);
+                dollar -= Number(params[2]);
             } else {
-                euro -= 3;
-                dollar += 5;
+                euro -= Number(params[4]);
+                dollar += Number(params[3]);
             }
             candy--;
             if (!candy && euro) return 'Không tính được';
@@ -112,7 +111,8 @@ answerBtn.forEach(function (btn) {
         if (!parentElement.querySelector('.answer')) {
             var answer = document.createElement('div');
             answer.classList.add('answer');
-            answer.innerText = functionQuesstion[e.target.getAttribute('function')]();
+            let params = e.target.getAttribute('data').split('+');
+            answer.innerText = functionQuesstion[e.target.getAttribute('function')](params);
             parentElement.append(answer)
         }
     }
