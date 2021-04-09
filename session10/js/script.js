f53494e46105a1247f3e5098a0e0fe83ca919444
huyhuanhg <huyhuanhg@gmail.com>
2021-04-09 11:38:53 +0700

    may tinh

--- session10/js/script.js
+++ session10/js/script.js
@@ -22,14 +22,46 @@
 //     document.write('<br/>')
 // }
 // cách 2
-for (var i = 0; i <= 100; i++) {
-    if (!(i % 15)) {
-        document.write(`${i} chia het cho 15`);
-    } else if (!(i % 5)) {
-        document.write(`${i} chia het cho 5`);
-    } else if (!(i % 3)) {
-        document.write(`${i} chia het cho 3`);
-    } else
-        document.write(`${i} Khong chia het cho 3, 5 va 15`);
-    document.write('<br/>')
+// for (var i = 0; i <= 100; i++) {
+//     if (!(i % 15)) {
+//         document.write(`${i} chia het cho 15`);
+//     } else if (!(i % 5)) {
+//         document.write(`${i} chia het cho 5`);
+//     } else if (!(i % 3)) {
+//         document.write(`${i} chia het cho 3`);
+//     } else
+//         document.write(`${i} Khong chia het cho 3, 5 va 15`);
+//     document.write('<br/>')
+// }
+
+
+var day = prompt();
+var x;
+switch (Number(day)) {
+    case 2:
+        x = 'month day';
+        break;
+    case 3:
+        x = 'thu 3';
+        break;
+    case 4:
+        x = 'thu 4';
+        break;
+    case 5:
+        x = 'thu 5';
+        break;
+    case 6:
+        x = 'thu 6';
+        break;
+    case 7:
+        x = 'thu 7';
+        break;
+    case 8:
+        x = 'sunday';
+        break;
+    default:
+        x = 'no day';
+        break;
 }
+
+document.write(x);
